9bac8701e8da63e18830dacda3af07aee3f90891
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-05-10 13:43:55 -0400

    just some stuff i guess

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is a test app I created to help me figure out why Heroku does not like my Portfolio site.
+
+          HEROKU BE NICE TO ME OKAY BYE NO MORE H10 ERROR THX
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
