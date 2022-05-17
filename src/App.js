2a636d034463e11c41d80d03bcb4bc08d13f134a
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-05-16 21:09:34 -0400

    view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Nav from './components/HeadersFooters/Nav/index.js';
-// import View from './components/View/index.js';
+import View from './components/View/index.js';
 // import Footer from "./components/HeadersFooters/Footer/index.js";
 // // import './App.css';
 import Clouds1 from './assets/videos/clouds1.mp4';
@@ -21,10 +21,10 @@ function App() {
       <video id="background-video" autoPlay loop muted>
         <source src={Clouds1} type="video/mp4" />
       </video>
-      {/* <main>
+      <main>
         <View />
       </main>
-      <Footer /> */}
+      {/* <Footer /> */}
       welp
     </div>
   );
