131cdec526d4e85eb301efd8c363348ab4cf7d1f
jaredinyaagha1 <jaredinyaagha@gmail.com>
2022-07-13 17:17:29 -0400

    added blurbs

--- src/components/Links/AboutLinks/index.js
+++ src/components/Links/AboutLinks/index.js
@@ -5,22 +5,22 @@ function AboutLinks() {
 
     const [AboutLinks] = useState([
         {
-            title: "omg omg omg omg",
-            statement: "omg omg omg omg",
+            title: "2 Years of Experience",
+            statement: "in Electrical Engineering",
             icon: <svg xmlns="http://www.w3.org/2000/svg" width="" height="" fill="currentColor" class="bi bi-activity" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z" />
             </svg>
         },
         {
-            title: "omg omg omg omg",
-            statement: "omg omg omg omg",
+            title: "Certified Full Stack",
+            statement: "Software Engineer",
             icon: <svg xmlns="http://www.w3.org/2000/svg" width="" height="" fill="currentColor" class="bi bi-arrow-through-heart-fill" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2.854 15.854A.5.5 0 0 1 2 15.5V14H.5a.5.5 0 0 1-.354-.854l1.5-1.5A.5.5 0 0 1 2 11.5h1.793l3.103-3.104a.5.5 0 1 1 .708.708L4.5 12.207V14a.5.5 0 0 1-.146.354l-1.5 1.5ZM16 3.5a.5.5 0 0 1-.854.354L14 2.707l-1.006 1.006c.236.248.44.531.6.845.562 1.096.585 2.517-.213 4.092-.793 1.563-2.395 3.288-5.105 5.08L8 13.912l-.276-.182A23.825 23.825 0 0 1 5.8 12.323L8.31 9.81a1.5 1.5 0 0 0-2.122-2.122L3.657 10.22a8.827 8.827 0 0 1-1.039-1.57c-.798-1.576-.775-2.997-.213-4.093C3.426 2.565 6.18 1.809 8 3.233c1.25-.98 2.944-.928 4.212-.152L13.292 2 12.147.854A.5.5 0 0 1 12.5 0h3a.5.5 0 0 1 .5.5v3Z" />
             </svg>
         },
         {
-            title: "omg omg omg omg",
-            statement: "omg omg omg omg",
+            title: "Unyielding Passion",
+            statement: "For Software & Electronics",
             icon: <svg xmlns="http://www.w3.org/2000/svg" width="" height="" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
                 <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001" />
             </svg>
